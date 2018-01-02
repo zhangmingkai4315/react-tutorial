3be48d9ff977d496cb48697b3f9e7c9dc64cda29
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-01-02 22:47:18 +0800

    add nav route

--- httprequest-project/src/App.js
+++ httprequest-project/src/App.js
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
+import { BrowserRouter }from 'react-router-dom';
 import './App.css';
 import Blogs from './Blogs/Blogs';
-import FullBlog from './Blogs/FullBlog/FullBlog'
 import NewBlog from './Blogs/NewBlog/NewBlog';
+import {Route} from 'react-router-dom'
+import Aux from './HOC/Aux'
+import Link from 'react-router-dom/Link';
 class App extends Component {
-  state = {
-    choosedBlogId:null
-  }
-  chooseBlog=(id)=>{
-    if(typeof id==='number' && id>0){
-      this.setState({choosedBlogId:id})
-    }
-  }
-  deleteBlog=(id)=>{
-    console.log('delete '+id)
-    axios.delete('/posts/' + id)
-         .then(response=>{
-           console.log(response)
-         })
-  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Blogs chooseBlog={this.chooseBlog}/>
-        <FullBlog 
-            blogId={this.state.choosedBlogId}
-            onDeleteBlog={this.deleteBlog}
-            />
-        <NewBlog/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <nav>
+              <ul>
+                <li><Link to="/blogs">blogs</Link></li>
+                <li><Link to="/new">new</Link></li>
+              </ul>
+            </nav>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Route path="/blogs" component={Blogs}/>
+          <Route path="/new" exact component={NewBlog}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
